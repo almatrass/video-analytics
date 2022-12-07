3a303c8f720c4d4a6e0ad26ea5464143f976cc67
40259039 <rflynn12@qub.ac.uk>
2022-12-07 19:21:44 +0000

    Add SVM with full image and HOG

---
+++ Classifiers/SVMWithHOG.m
@@ -0,0 +1,31 @@
+% Load all positive and negative examples
+images=imageDatastore('Resources/images\','IncludeSubfolders',true,'LabelSource','foldernames');
+
+% Define the feature extraction method to be used
+extractorFunction = @extractHOGFeatures;
+
+% Split the dataset randomly into training and testing sets, with 75% used
+% for training and 25% for testing
+[trainingSet, testingSet] = splitEachLabel(images, 0.75, 'randomize');
+
+% Define options to be used by SVM classifier (if any)
+%SVMOptions = templateSVM("KernelFunction", "linear");
+
+% Training
+tic;
+bag = bagOfFeatures(trainingSet,"CustomExtractor", extractorFunction);
+categoryClassifier = trainImageCategoryClassifier(trainingSet, bag, "LearnerOptions", SVMOptions);
+trainingTime = toc;
+
+% Testing
+tic;
+
+% Run classifier on the testing set
+confMatrix = evaluate(categoryClassifier, testingSet);
+
+% Determine accuracy of classifier on testing set
+testingAccuracy = mean(diag(confMatrix));
+
+testingTime = toc;
+
+save Detector\SVMHOGDetectorModel categoryClassifier
