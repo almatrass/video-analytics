607b38b51503668969d39a6c8328c519d1419c7b
40259039 <rflynn12@qub.ac.uk>
2022-12-07 19:23:25 +0000

    Add SVM classifiers with MSER and SIFT

---
+++ Classifiers/SVMWithSIFT.m
@@ -0,0 +1,34 @@
+% Load all positive and negative examples
+images=imageDatastore('Resources/images','IncludeSubfolders',true,'LabelSource','foldernames');
+
+% Define the feature extraction method to be used
+extractorFunction = @extractSIFTFeatures;
+
+% Split the dataset randomly into training and testing sets, with 60% used
+% for training and 40% for testing
+[trainingSet, testingSet] = splitEachLabel(images, 0.6, 'randomize');
+
+% Define options to be used by SVM classifier (if any)
+SVMOptions = templateSVM("KernelFunction", "linear");
+
+% Training
+tic;
+bag = bagOfFeatures(trainingSet,"CustomExtractor", extractorFunction);
+% categoryClassifier = trainImageCategoryClassifier(trainingSet, bag);
+categoryClassifier = trainImageCategoryClassifier(trainingSet, bag, "LearnerOptions", SVMOptions);
+trainingTime = toc;
+
+% Testing
+tic;
+
+% Run classifier on the testing set
+confMatrix = evaluate(categoryClassifier, testingSet);
+
+% Determine accuracy of classifier on testing set
+testingAccuracy = mean(diag(confMatrix));
+
+testingTime = toc;
+
+% As this is the chosen method for the detector, the model is saved to be
+% used by the detector
+save Detector\SVMSIFTDetectorModel categoryClassifier
